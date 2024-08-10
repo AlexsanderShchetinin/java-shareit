47d60e2da018987348946b8cb6e9df96d0f42f97
Sportik99 <shchetinin.ase@yandex.ru>
2024-08-11 01:01:28 +0600

    fix: change version spring-boot-starter-parent pom.xml

--- src/main/java/ru/practicum/shareit/booking/mapper/BookingMapper.java
+++ src/main/java/ru/practicum/shareit/booking/mapper/BookingMapper.java
@@ -2,12 +2,9 @@ package ru.practicum.shareit.booking.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Named;
 import ru.practicum.shareit.booking.Booking;
 import ru.practicum.shareit.booking.dto.BookingCreatingDto;
 import ru.practicum.shareit.booking.dto.BookingDto;
-import ru.practicum.shareit.item.model.Item;
-import ru.practicum.shareit.user.User;
 
 @Mapper(componentModel = "spring", uses = {BookingItemConverter.class, BookingUserConverter.class})
 public interface BookingMapper {
@@ -23,20 +20,5 @@ public interface BookingMapper {
     BookingDto toDto(Booking booking);
 }
 
-@Mapper(componentModel = "spring")
-interface BookingItemConverter {
 
-    @Named("toItem")
-    @Mapping(target = "id", source = "id")
-    Item toItem(Long id);
 
-}
-
-@Mapper(componentModel = "spring")
-interface BookingUserConverter {
-
-    @Named("toUser")
-    @Mapping(target = "id", source = "id")
-    User toUser(Long id);
-
-}
