9b7127cb38101cf1d0201724fda61f0049c3fa0d
Sportik99 <shchetinin.ase@yandex.ru>
2024-08-12 21:31:49 +0600

    fix: change fields model objects in Dto classes on Dto objects

--- src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
@@ -4,7 +4,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import ru.practicum.shareit.item.model.Item;
 
 import java.time.LocalDateTime;
 
@@ -20,7 +19,7 @@ public class CommentDto {
 
     private Long id;
     private String text;
-    private Item item;
+    private ItemDto item;
     private String authorName;
     private LocalDateTime created;
 
